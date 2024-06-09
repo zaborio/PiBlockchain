32db7f588ff108dc4b8fbfdd125fe8612c726c28
zaborio <94500847+zaborio@users.noreply.github.com>
2024-06-09 10:24:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,204 +1,99 @@
-async function loadBlockchain() {
-    if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-        await window.ethereum.enable();
-    } else {
-        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
-        return;
+import hashlib
+import time
+from decimal import Decimal, getcontext
+
+class Block {
+    constructor(index, timestamp, data, previousHash) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.hashBlock();
     }
 
-    const contractAddress = 'YOUR_CONTRACT_ADDRESS';  // Remplacez par l'adresse de votre contrat
-    const abi = [
-        {
-            "inputs": [],
-            "name": "getBlockchainLength",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "index",
-                    "type": "uint256"
-                }
-            ],
-            "name": "getBlock",
-            "outputs": [
-                {
-                    "components": [
-                        {
-                            "internalType": "uint256",
-                            "name": "index",
-                            "type": "uint256"
-                        },
-                        {
-                            "internalType": "uint256",
-                            "name": "timestamp",
-                            "type": "uint256"
-                        },
-                        {
-                            "internalType": "string",
-                            "name": "data",
-                            "type": "string"
-                        },
-                        {
-                            "internalType": "bytes32",
-                            "name": "previousHash",
-                            "type": "bytes32"
-                        },
-                        {
-                            "internalType": "bytes32",
-                            "name": "hash",
-                            "type": "bytes32"
-                        }
-                    ],
-                    "internalType": "struct PiBlockchain.Block",
-                    "name": "",
-                    "type": "tuple"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "data",
-                    "type": "string"
-                },
-                {
-                    "internalType": "bytes32",
-                    "name": "previousHash",
-                    "type": "bytes32"
-                }
-            ],
-            "name": "addBlock",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ];
-
-    const contract = new web3.eth.Contract(abi, contractAddress);
-    const accounts = await web3.eth.getAccounts();
-
-    const blockchainLength = await contract.methods.getBlockchainLength().call();
-    const blockchainDiv = document.getElementById('blockchain');
-    blockchainDiv.innerHTML = '';
-
-    for (let i = 0; i < blockchainLength; i++) {
-        const block = await contract.methods.getBlock(i).call();
-        const blockElement = document.createElement('div');
-        blockElement.innerHTML = `
-            <h3>Block ${block.index}</h3>
-            <p>Timestamp: ${block.timestamp}</p>
-            <p>Data: ${block.data}</p>
-            <p>Previous Hash: ${block.previousHash}</p>
-            <p>Hash: ${block.hash}</p>
-        `;
-        blockchainDiv.appendChild(blockElement);
+    hashBlock() {
+        const sha = crypto.createHash('sha256');
+        sha.update(`${this.index}${this.timestamp}${this.data}${this.previousHash}`);
+        return sha.digest('hex');
     }
 }
 
-async function addBlock() {
-    const contractAddress = 'YOUR_CONTRACT_ADDRESS';  // Remplacez par l'adresse de votre contrat
-    const abi = [
-        {
-            "inputs": [],
-            "name": "getBlockchainLength",
-            "outputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "",
-                    "type": "uint256"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "uint256",
-                    "name": "index",
-                    "type": "uint256"
-                }
-            ],
-            "name": "getBlock",
-            "outputs": [
-                {
-                    "components": [
-                        {
-                            "internalType": "uint256",
-                            "name": "index",
-                            "type": "uint256"
-                        },
-                        {
-                            "internalType": "uint256",
-                            "name": "timestamp",
-                            "type": "uint256"
-                        },
-                        {
-                            "internalType": "string",
-                            "name": "data",
-                            "type": "string"
-                        },
-                        {
-                            "internalType": "bytes32",
-                            "name": "previousHash",
-                            "type": "bytes32"
-                        },
-                        {
-                            "internalType": "bytes32",
-                            "name": "hash",
-                            "type": "bytes32"
-                        }
-                    ],
-                    "internalType": "struct PiBlockchain.Block",
-                    "name": "",
-                    "type": "tuple"
-                }
-            ],
-            "stateMutability": "view",
-            "type": "function"
-        },
-        {
-            "inputs": [
-                {
-                    "internalType": "string",
-                    "name": "data",
-                    "type": "string"
-                },
-                {
-                    "internalType": "bytes32",
-                    "name": "previousHash",
-                    "type": "bytes32"
-                }
-            ],
-            "name": "addBlock",
-            "outputs": [],
-            "stateMutability": "nonpayable",
-            "type": "function"
-        }
-    ];
-
-    const contract = new web3.eth.Contract(abi, contractAddress);
-    const accounts = await web3.eth.getAccounts();
-    const blockchainLength = await contract.methods.getBlockchainLength().call();
-    const previousBlock = await contract.methods.getBlock(blockchainLength - 1).call();
-    const data = 'New Block     const previousHash = previousBlock.hash;
-
-    await contract.methods.addBlock(data, previousHash).send({ from: accounts[0] });
-    loadBlockchain();
+function bbpTerm(k) {
+    k = new Decimal(k);
+    const term = new Decimal(1).div(Decimal.pow(16, k)).times(
+        new Decimal(4).div(8 * k + 1)
+        .minus(new Decimal(2).div(8 * k + 4))
+        .minus(new Decimal(1).div(8 * k + 5))
+        .minus(new Decimal(1).div(8 * k + 6))
+    );
+    return term;
 }
 
-window.onload = loadBlockchain;
+function calculatePiBbp(precision) {
+    getcontext().prec = precision + 10;
+    const pi = Array.from({ length: precision }, (_, k) => bbpTerm(k))
+        .reduce((acc, term) => acc.plus(term), new Decimal(0));
+    return [pi.toString(), pi];
+}
+
+function hashSegment(segment) {
+    const sha = crypto.createHash('sha256');
+    sha.update(segment);
+    return sha.digest('hex');
+}
+
+function createGenesisBlock() {
+    return new Block(0, Date.now(), "Genesis Block", "0");
+}
+
+function nextBlock(lastBlock, piSegment, piHash) {
+    const thisIndex = lastBlock.index + 1;
+    const thisTimestamp = Date.now();
+    const thisData = {
+        segment: piSegment,
+        hash: piHash,
+    };
+    const thisHash = lastBlock.hash;
+    return new Block(thisIndex, thisTimestamp, thisData, thisHash);
+}
+
+function displayPiValue(piApprox, precision) {
+    const truncatedPi = piApprox.toString().substring(0, 10);
+    console.log(`Approximate value of pi: ${truncatedPi}... with ${precision} decimal places`);
+}
+
+document.getElementById('startMining').addEventListener('click', () => {
+    const initialPrecision = 5;
+    const incrementFactor = 2;
+    const updateInterval = 10000;  // 10 seconds
+
+    const blockchain = [createGenesisBlock()];
+    let previousBlock = blockchain[0];
+
+    let precision = initialPrecision;
+    let startTime = Date.now();
+
+    (function minePi() {
+        const [piSegment, piApprox] = calculatePiBbp(precision);
+        const piHash = hashSegment(piSegment);
+
+        const blockToAdd = nextBlock(previousBlock, piSegment, piHash);
+        blockchain.push(blockToAdd);
+        previousBlock = blockToAdd;
+
+        console.log(`Block #${blockToAdd.index} has been added to the blockchain!`);
+        console.log(`Segment: ${piSegment.substring(0, 50)}...`);
+        console.log(`Hash: ${blockToAdd.hash}`);
+
+        precision *= incrementFactor;
+
+        if (Date.now() - startTime >= updateInterval) {
+            displayPiValue(piApprox, precision);
+            startTime = Date.now();
+        }
+
+        document.getElementById('miningStatus').innerText = `Last Block: #${blockToAdd.index}`;
+
+        setTimeout(minePi, 1000);
+    })();
+});
